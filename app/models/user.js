e5c8d606708583914615f8b6dab5789e03fd9317
Hack Reactor Students <acjones617@gmail.com>
2014-05-12 16:38:17 -0700

    Handles new users table

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,35 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
+  tableName: 'users',
+
+  hasTimestamps: true,
+
+  initialize: function() {
+    console.log('trying to intialize new user');
+    //this.on('creating', function(model, attrs, options) {
+  }
 });
 
+
 module.exports = User;
+
+// var Link = db.Model.extend({
+//   tableName: 'urls',
+//   hasTimestamps: true,
+//   defaults: {
+//     visits: 0
+//   },
+//   clicks: function() {
+//     return this.hasMany(Click);
+//   },
+//   initialize: function(){
+//     this.on('creating', function(model, attrs, options){
+//       var shasum = crypto.createHash('sha1');
+//       shasum.update(model.get('url'));
+//       model.set('code', shasum.digest('hex').slice(0, 5));
+//     });
+//   }
+// });
+
+// module.exports = Link;
