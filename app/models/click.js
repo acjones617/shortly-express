15ed680cb1d60618ccd63f4c4887369fa3723f36
Hack Reactor Students <acjones617@gmail.com>
2014-05-13 14:25:38 -0700

    Starts to alter logic behind how links are displayed

--- app/models/click.js
+++ app/models/click.js
@@ -1,11 +1,15 @@
 var db = require('../config');
-var Link = require('./link.js')
+var Link = require('./link.js');
+var User = require('./user.js');
 
 var Click = db.Model.extend({
   tableName: 'clicks',
   hasTimestamps: true,
   link: function() {
     return this.belongsTo(Link, 'link_id');
+  },
+  user: function() {
+    return this.belongsTo(User, 'user_id');
   }
 });
 
