df22894fd31d0d9c003f21bc579aee6615e9547b
Hack Reactor Students <acjones617@gmail.com>
2014-05-13 22:27:19 -0700

    Adds user specific data to database

--- app/models/user.js
+++ app/models/user.js
@@ -2,6 +2,7 @@ var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 var Link = require('./link.js');
+var Click = require('./click.js');
 
 var User = db.Model.extend({
   tableName: 'users',
@@ -9,15 +10,21 @@ var User = db.Model.extend({
   hasTimestamps: true,
 
   initialize: function() {
-    bcrypt.hash(this.get('password'), null, null, function(err, hash) {
-      this.set('password', hash);
-      this.save();
-    }.bind(this));
+    if (this.get('password') !== '') {
+      bcrypt.hash(this.get('password'), null, null, function(err, hash) {
+        this.set('password', hash);
+        this.save();
+      }.bind(this));
+    }
 
   },
 
   link: function() {
     return this.hasMany(Link);
+  },
+
+  click: function() {
+    return this.hasMany(Click);
   },
 
   correctPass: function(password, cb) {
