d58a5939a6d55eea98591397aaaf7589588ff594
Hack Reactor Students <acjones617@gmail.com>
2014-05-13 22:22:08 -0700

    Changes schema for database to handle user-specific data

--- app/config.js
+++ app/config.js
@@ -51,8 +51,8 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
 db.knex.schema.hasTable('users').then(function(exists) {
   if (!exists) {
     db.knex.schema.createTable('users', function(user) {
-      user.increments('id').primary();
-      user.string('username', 100);
+      user.increments('id');
+      user.string('username', 100).unique();
       user.string('password', 100);
       user.timestamps();
     }).then(function (table) {
